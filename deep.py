316c7a7ebe198f8b714913360fa290a237b26f1b
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-07-17 11:09:53 +0200

    Finished deep.py project

---
+++ deep.py
@@ -0,0 +1,15 @@
+#Asking the user for the answer to the following question
+input("What is the answer to the Great Question of Life, the Universe and Everything?: ")
+
+#Setting up an if statement to determine the output.
+if " " == ("42") :
+    print ("Yes")
+elif " " == "Forty-two" :
+    print ("Yes")
+elif " " == "Forty two" :
+    print Yes
+
+else:
+    print ("No")
+
+
