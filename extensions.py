400de23f6de961f3672b30b05a446b520b64e33e
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-07-23 14:24:10 +0200

    Created a separate rule to extract the extensions once at the start. So that I do not have to do it every time, and so that it checks the extension, not the entire filename.

--- extensions.py
+++ extensions.py
@@ -2,20 +2,31 @@
 
 filename = input("Please enter your file name: ").lower().strip()
 
+#Extracting the extension from the file.
+
+extension = filename.split(".")[-1]
+
 #Setting up a rule in which the user's input will be tested against.
 
 #if the file name ends in the following strings then the system will print the text "image/" followed by the user's input, splitting the user's input at
 #the period . With "-1" grabbing the last part of the file, the extension.
+
 if filename.endswith(('.gif', '.png', '.jpeg', '.jpg')):
     if filename == "jpg":
         filename = "jpeg"
-    print(f'image/{filename.split(".")[-1]}')
+    print(f'image/{extension}')
+
 #The same step is repeated below, except I am adding "application" instead of "image" as the following extensions are not images, rather applications.
+
 elif filename.endswith(('.pdf', '.zip')):
-    print(f'application/{filename.split(".")[-1]}')
+    print(f'application/{extension}')
+
 #The same step is repeated below, except I am adding "text" instead of anything else as the following extensions are not images, rather plain text files.
+
 elif filename.endswith('.txt'):
     print("text/plain")
+
 #Finally I am setting a rule so that any other user input will output the following application.
+
 else:
     print("application/octet-stream")
