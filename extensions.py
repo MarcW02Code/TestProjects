5e35ca014e6508257abfebdfee68f3bae28cdc28
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-07-23 14:18:37 +0200

    Changed the .split() to .strip as I got confused. Added a rule to prevent incorrect outputs with "jpg" and "jpeg"

--- extensions.py
+++ extensions.py
@@ -1,12 +1,14 @@
 #Prompting the user for a file name
 
-filename = input("Please enter your file name: ").lower().split()
+filename = input("Please enter your file name: ").lower().strip()
 
 #Setting up a rule in which the user's input will be tested against.
 
 #if the file name ends in the following strings then the system will print the text "image/" followed by the user's input, splitting the user's input at
 #the period . With "-1" grabbing the last part of the file, the extension.
 if filename.endswith(('.gif', '.png', '.jpeg', '.jpg')):
+    if filename == "jpg":
+        filename = "jpeg"
     print(f'image/{filename.split(".")[-1]}')
 #The same step is repeated below, except I am adding "application" instead of "image" as the following extensions are not images, rather applications.
 elif filename.endswith(('.pdf', '.zip')):
