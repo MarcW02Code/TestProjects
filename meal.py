d75940b2ba1c005cd20045453484f07fb3b015e5
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-07-25 11:28:21 +0200

    Finished meal program

--- meal.py
+++ meal.py
@@ -1,14 +1,36 @@
 #Defining the functions
 
 def main():
+    #Getting the input
     answer = input("What time is it?: ")
-    return
-    ...
+    float_time = convert(answer)
+    if 7 <= float_time <=8:
+        print("breakfast time")
+    elif 12 <= float_time <= 13:
+        print("lunch time")
+    elif 18 <= float_time <= 19:
+        print("dinner time")
+    else:
+        print("no meal time")
 
 
+    
+
+#Converting the time to a float number
 def convert(time):
-    ...
+    hours, minutes = time.split(":")
+    hours = float(hours)
+    if minutes.endswith("p.m") == True:
+        actual_minute,format = minutes.split(" ")
+        hours = (hours + 12)
+        new_minutes = float(actual_minute)/ 60
+    elif minutes.endswith("a.m") == True:
+        actual_minute, format =  minutes.split(" ")
+        new_minutes = float(actual_minute)/ 60
+    else:
+        new_minutes = float(minutes)/ 60
 
+    return hours + new_minutes
 
 if __name__ == "__main__":
     main()
