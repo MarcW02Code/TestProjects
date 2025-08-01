4aa588a7f30a01739d5e55f6455b2af791404bb7
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-01 16:43:12 +0200

    Finished final version of camel.py

--- camel.py
+++ camel.py
@@ -7,15 +7,27 @@ camelcase = input("Please enter the name of your variable, keep in mind it must 
 
 print("snake_case: ", end="")
 
+#Adding a flag to remember if the previous character in the for loop was uppercase
+
+was_prev_upper = False
+is_first_char = True
+
 #Setting up a for loop to go through each character in the input string.
 
 for c in camelcase:
 #if the character is in lowercase, print the character directly.
     if c.islower():
         print(c, end="")
+        was_prev_upper = False #Resetting the flag
+        is_first_char = False
 
-#If the character is uppercase, convert it to lowercase but make sure to add an underscore before it, then print it.
-    else:
-        print("_" + c.lower(), end="") 
+    elif c.isupper():
+        if not was_prev_upper and not is_first_char:
+            print("_" + c.lower(), end="")
+#If the character is uppercase, convert it to lowercase.
+        else:
+            print(c.lower(), end="") 
+        was_prev_upper = True
+        is_first_char = False
 
 print("")
