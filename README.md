57bb097cee027148ddbfcab83ceaeef429569698
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-01 16:13:48 +0200

    Finished first submission of camel.py, and edited the README accordingly.

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 Testing to see if github is easier for me to use for these test projects than gitlab.
 
 # Problem set 2, question 1:
-The way I want to approach this is by setting up a command that prompts the user for a variable name, and will end up outputting the variable's name in snake case instead of
-camel case.
+The way I want to approach this is by setting up a command that prompts the user for a variable name, called camelCase, and will end up outputting the variable's name in snake case instead of in camel case.
+After that line I want to print out the snake case version of the camel case word, but I am making use of the 'end=""' argument to make sure that each changed letter goes to the end of the line instead of starting a new line. So that when the print command runs again it continues on the same line.
+
 
 
 
--- camel.py
+++ camel.py
@@ -1,3 +1,21 @@
 #Prompting the user for their input
 
-camelCase = input("Please enter the name of your variable, keep in mind it must be in camel case: ")
+camelcase = input("Please enter the name of your variable, keep in mind it must be in camel case: ")
+
+#Printing the final output, starting with the label "snake_case", and making sure to continue the argument on the same
+#line instead of starting a new one each time.
+
+print("snake_case: ", end="")
+
+#Setting up a for loop to go through each character in the input string.
+
+for c in camelcase:
+#if the character is in lowercase, print the character directly.
+    if c.islower():
+        print(c, end="")
+
+#If the character is uppercase, convert it to lowercase but make sure to add an underscore before it, then print it.
+    else:
+        print("_" + c.lower(), end="") 
+
+print("")
