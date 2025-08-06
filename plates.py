ae3815bc69c28f7e6060d99a4ad4c45fe4858bf0
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-06 18:24:47 +0200

    testing final program

--- plates.py
+++ plates.py
@@ -10,8 +10,26 @@ def main():
 
 
 def is_valid(s):
-    if 
+    if len(s) > 6 or len(s) < 2:
+        return False
+    if not s[0:2].isalpha():
+        return False
+    for i in range(len(s)):
+        if s[i].isdigit():
+            first_number_index = i
+            break
+    if first_number_index != -1:
+        if s[first_number_index] == '0':
+            return False
+        right_part = s[first_number_index:]
+        if not right_part.isdigit():
+            return False
+    
+    if not s.isalnum():
+        return False
 
+    return True
 
+    
 
 main()
