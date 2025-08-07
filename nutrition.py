f95376bad7e64b7c894ace333f1f6bc0d618b739
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-07 16:05:40 +0200

    neatened it up and kept coding

--- nutrition.py
+++ nutrition.py
@@ -1,5 +1,35 @@
-#Creating a dictionary
-def main():
-    fruits = {"Apple": "130", "Avocado": "50", "Banana": "110", "Cantaloupe": "50", "Grapefruit": "60", "Grapes": "90", "Honeydew Melon": "50", "Kiwifruit": "90", "Lemon": "15", "Lime": "20", "Nectarine": "60", "Orange": "80", "Peach": "60", "Pear": "100", "Pineapple": "50", "Plums": "70", "Strawberries": "50", "Sweet Cherries": "100", "Tangerine": "50", "Watermelon": "80" }
+#User input
 
 userinput = input("Please state the fruit in question: ")
+
+#Creating a dictionary
+def main():
+    fruits = {
+        "Apple": 130, 
+        "Avocado": 50, 
+        "Banana": 110, 
+        "Cantaloupe": 50, 
+        "Grapefruit": 60, 
+        "Grapes": 90, 
+        "Honeydew Melon": 50, 
+        "Kiwifruit": 90, 
+        "Lemon": 15, 
+        "Lime": 20, 
+        "Nectarine": 60, 
+        "Orange": 80, 
+        "Peach": 60, 
+        "Pear": 100, 
+        "Pineapple": 50, 
+        "Plums": 70, 
+        "Strawberries": 50, 
+        "Sweet Cherries": 100, 
+        "Tangerine": 50, 
+        "Watermelon": 80
+     }
+    print(fruit_calories(fruits))
+
+def fruit_calories(fruits):
+    return 
+
+
+
