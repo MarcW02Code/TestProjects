4bab50d347a25dcdc547944a8f5c96dce98a0182
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-07 17:57:24 +0200

    finished fuel v2

--- fuel.py
+++ fuel.py
@@ -1,32 +1,37 @@
 #Take in an input
 
-fuel = input("Fraction: ")
+while True:
+    fuel = input("Fraction: ")
 
 #Make sure the input is formatted as x/y, wherein x and y are both positive integers, and
 #outputs as a percentage how much fuel is in the tank, rounded to the nearest integer.
 
-try:
-    x, y = fuel.split("/")
-    x = int(x)
-    y = int(y)
+    try:
+        x, y = fuel.split("/")
+        x = int(x)
+        y = int(y)
 
-    x = x * 100
-    p = x / y
-    p = round(p)
+        x = x * 100
+        p = x / y
+        p = round(p)
 
-    if 100 >= p >= 99:
-        print(f"F")
-        break
+        if 100 >= p >= 99:
+            print(f"F")
+            break
 
-    elif p > 100:
+        elif p > 100:
+            pass
+
+        elif p <= 1:
+            print(f"E")
+            break
+
+        else:
+            print(f"{p}%")
+            break
+
+    except(ValueError, ZeroDivisionError):
         pass
-
-    elif p <+ 1:
-        print(f"E")
-        break
-
-except(ValueError, ZeroDivisionError):
-    pass
     
 
 
