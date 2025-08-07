9f16639079f7f0340a14cadd503d36e9a59648e2
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-07 16:22:01 +0200

    First finished program

--- nutrition.py
+++ nutrition.py
@@ -1,33 +1,36 @@
-#User input
-
-userinput = input("Please state the fruit in question: ")
-
 #Creating a dictionary
-def main():
-    fruit_calories = {
-        "Apple": 130, 
-        "Avocado": 50, 
-        "Banana": 110, 
-        "Cantaloupe": 50, 
-        "Grapefruit": 60, 
-        "Grapes": 90, 
-        "Honeydew Melon": 50, 
-        "Kiwifruit": 90, 
-        "Lemon": 15, 
-        "Lime": 20, 
-        "Nectarine": 60, 
-        "Orange": 80, 
-        "Peach": 60, 
-        "Pear": 100, 
-        "Pineapple": 50, 
-        "Plums": 70, 
-        "Strawberries": 50, 
-        "Sweet Cherries": 100, 
-        "Tangerine": 50, 
-        "Watermelon": 80
-     }
-    
 
+fruits = {
+    "Apple": "Calories: 130", 
+    "Avocado": "Calories: 50", 
+    "Banana": "Calories: 110", 
+    "Cantaloupe": "Calories: 50", 
+    "Grapefruit": "Calories: 60", 
+    "Grapes": "Calories: 90", 
+    "Honeydew Melon": "Calories: 50", 
+    "Kiwifruit": "Calories: 90", 
+    "Lemon": "Calories: 15", 
+    "Lime": "Calories: 20", 
+    "Nectarine": "Calories: 60", 
+    "Orange": "Calories: 80", 
+    "Peach": "Calories: 60", 
+    "Pear": "Calories: 100", 
+    "Pineapple": "Calories: 50", 
+    "Plums": "Calories: 70", 
+    "Strawberries": "Calories: 50", 
+    "Sweet Cherries": "Calories: 100", 
+    "Tangerine": "Calories: 50", 
+    "Watermelon": "Calories: 80"
+}
+
+#Setting up user input, using strip() and title() to make sure
+#the name is correctly formatted.
+
+#Used setdefault to check if the fruit is in the list and if not
+#return the text "Not a fruit"
+fruit = input("Fruit: ").strip().title()
+print(fruits.setdefault(fruit, "Not a fruit"))
+    
 
 
 
