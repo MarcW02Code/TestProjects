d59eaf84cc065af6e946acfcac3bbd2ebb1a9ba4
MarcOs_02:gti config --global user.email wannenburghmarc@gmail.comgit config --global user.name MarcOs_02 <wannenburghmarc@gmail.com>
2025-08-07 22:42:54 +0200

    added the menu

--- taqueria.py
+++ taqueria.py
@@ -2,6 +2,20 @@
 # Then when the user has finished their order they must input control-d 
 # which will end their input to the program.
 
+def main():
+    menu = 
+{    
+    Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
+
 while true:
     try:
         item = input("Item:\n")
